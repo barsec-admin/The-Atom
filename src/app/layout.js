766b2533ca6388aa5024a286c783d84c93e49231
Seth Shuey <8906544+SethShuey1@users.noreply.github.com>
2024-12-14 00:31:09 -0600

    2024-12-14 00:17 took from 2024-11-2 15:53 and made changes to apparance because login was working

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#ECE7CA]">
+      <body className="bg-[#ECE7CA] text-black">
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
